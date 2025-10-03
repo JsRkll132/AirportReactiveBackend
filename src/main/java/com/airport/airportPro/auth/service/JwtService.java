4aa6f310667f09422fb4a384080326fbd91a6749
JsRkll132 <jose.apaza2@unmsm.edu.pe>
2025-10-03 11:08:45 -0500

    add env values for jwt token

--- src/main/java/com/airport/airportPro/auth/service/JwtService.java
+++ src/main/java/com/airport/airportPro/auth/service/JwtService.java
@@ -1,5 +1,29 @@
 package com.airport.airportPro.auth.service;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import com.airport.airportPro.auth.entity.User.User;
+
 public class JwtService {
 
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtTokenExpiration;
+
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long jwtRefreshTokenExpiration;
+
+
+    public String generateToken(final User user){
+        return buildToken(user, jwtTokenExpiration);
+    }
+    public String generateRefreshToken(final User user){
+        return buildToken(user, jwtRefreshTokenExpiration);
+    }
+
+    private String buildToken(final User user, final long time){
+        return "";
+    }
 }
